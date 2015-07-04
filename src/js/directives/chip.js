679bea0c6927cb350562ea3adc3311700cc80b89
Andy Kitson <andrew.kitson@gmail.com>
2015-07-04 16:44:42 -0500

    Adjust chip text color to contrast with background

--- src/js/directives/chip.js
+++ src/js/directives/chip.js
@@ -7,8 +7,27 @@ app.directive('ruiChip', [function () {
     templateUrl: 'templates/chip.html',
     scope: {
       name: '=',
-      color: '=',
+      backgroundColor: '=color',
       onRemove: '&',
+    },
+    link: function (scope, element, attrs) {
+      scope.style = {
+        'background-color': scope.backgroundColor
+      };
+
+      // adjust text color depending on background brightness
+      if (window.tinycolor) {
+        var dark = 'black', light = 'white';
+        var isDark = window.tinycolor(scope.backgroundColor).isDark();
+
+        scope.style.color = isDark ? light : dark;
+
+        // also make sure closing button (X) matches
+        scope.closeStyle = {
+          color: scope.style.color,
+          'text-shadow': '0 1px 0 ' + (isDark ? dark : light)
+        };
+      }
     }
   };
 }]);
